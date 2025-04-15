dd74c51a6e031e4789b9e427d993d6018c79e0df
junaidbinjaman <me@junaidbinjaman.com>
2025-04-15 17:36:16 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+>Hello As a human, we have our own responsibility to make money and shine
+
+
+saddasdasdasdsad sdfsdf <mark>sdfdsf</mark>
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const person: string = "hello world";
+console.log(person);
+
+function generateName<T>(name: T): string {
+    return 'Hello ' + name;
+}
+
+const beautiful_name = generateName<string>('Junaid Bin');
+console.log(beautiful_name);
